e878acd3360245e4e8792470b88597434e060feb
licongwen <649091795@qq.com>
2018-08-17 09:11:10 +0800

    新增vuex模块

--- src/components/image.vue
+++ src/components/image.vue
@@ -1,19 +1,34 @@
 <template>
     <div>
         <p class="title">image here;</p>
+        <input v-model="token"/>
     </div>
 </template>
 <script>
 import '../assets/index.css'
 export default {
     name:'ImagePage',
+    data(){
+        return {
+            token:'123456token',
+            userName:'',
+        }
+    },
+    watch:{
+        'token':function(val,oldVal){
+            console.log('current value: '+val);
+            console.log('origin value: ' +oldVal);
+        }
+    },
     created(){
         this.initData();
+        this.userName = this.$store.state.userName;
+        console.log(this.userName);
     },
     methods:{
         initData(){
             let arr = 12;
-            const str = 'licongwen';
+            const str = 'xiaodebaoasdd';
             console.log(str);
         }
     }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue"
 import VueRouter from "vue-router"
 import App from "./App.vue"
 import routerConfig from "./router"
+import store from './utils/store.js';
 
 Vue.use(VueRouter)
 
@@ -9,5 +10,6 @@ const router = new VueRouter(routerConfig)
 new Vue({
     el: "#app",
     router: router,
+    store,
     render: h => h(App)
 })
